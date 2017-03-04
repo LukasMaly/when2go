cfa83560274b203db35775eee60f99f30f7db905
Lukáš Malý <lukasmaly@gmail.com>
2017-03-04 20:01:43 +0100

    Initial commit

---
+++ functions.py
@@ -0,0 +1,23 @@
+import googlemaps
+from datetime import datetime
+
+
+def get_time_to_leave(home_address, work_address):
+    gmaps = googlemaps.Client(key='AIzaSyAoH4CMI1eRkZywJThDOl33rbJ96W1kn2Q')
+
+    now = datetime.now()
+    directions_result = gmaps.directions(home_address,
+                                         work_address,
+                                         mode="transit",
+                                         departure_time=now)
+
+    walking_duration = directions_result[0]['legs'][0]['steps'][0]['duration']['value']
+    departure_stop = directions_result[0]['legs'][0]['steps'][1]['transit_details']['departure_stop']['name']
+    departure_time = directions_result[0]['legs'][0]['steps'][1]['transit_details']['departure_time']['value']
+    line_name = directions_result[0]['legs'][0]['steps'][1]['transit_details']['line']['short_name']
+    arrival_stop = directions_result[0]['legs'][0]['steps'][-2]['transit_details']['arrival_stop']['name']
+
+    time_to_leave = departure_time - walking_duration
+    time_to_leave_str = datetime.fromtimestamp(time_to_leave).strftime('%Y-%m-%d %H:%M:%S')
+
+    return time_to_leave_str
---
+++ script.py
@@ -0,0 +1,7 @@
+from functions import get_time_to_leave
+
+
+home_address = 'YOUR_HOME_ADDRESS'
+work_address = 'YOUR_WORK_ADDRESS'
+
+print(get_time_to_leave(home_address, work_address))
